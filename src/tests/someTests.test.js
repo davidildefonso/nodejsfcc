ed20c7b60d721bfb185600a12f74aad9dcc145ff
David Ildefonso <david_lion_ig@outlook.com.pe>
2022-07-29 11:21:53 -0500

    fix test

--- src/tests/someTests.test.js
+++ src/tests/someTests.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { describe, it } = require('node:test');
 const assert = require('node:assert/strict');
 
--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -1,68 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const test = require('node:test');
 const assert = require('node:assert/strict');
 
-test('synchronous passing test', (t) => {
+test('synchronous passing test', (_t) => {
   // This test passes because it does not throw an exception.
   assert.strictEqual(1, 1);
 });
 
-test('synchronous failing test', (t) => {
-  // This test fails because it throws an exception.
-  assert.strictEqual(1, 2);
-});
-
-test('asynchronous passing test', async (t) => {
-  // This test passes because the Promise returned by the async
-  // function is not rejected.
-  assert.strictEqual(1, 1);
-});
-
-test('asynchronous failing test', async (t) => {
-  // This test fails because the Promise returned by the async
-  // function is rejected.
-  assert.strictEqual(1, 2);
-});
-
-test('failing test using Promises', (t) => {
-  // Promises can be used directly as well.
-  return new Promise((resolve, reject) => {
-    setImmediate(() => {
-      reject(new Error('this will cause the test to fail'));
-    });
-  });
-});
-
-test('callback passing test', (t, done) => {
+test('callback passing test', (_t, done) => {
   // done() is the callback function. When the setImmediate() runs, it invokes
   // done() with no arguments.
+  // eslint-disable-next-line @typescript-eslint/no-implied-eval, @typescript-eslint/no-unsafe-argument
   setImmediate(done);
 });
 
-test('callback failing test', (t, done) => {
-  // When the setImmediate() runs, done() is invoked with an Error object and
-  // the test fails.
-  setImmediate(() => {
-    done(new Error('callback failure'));
-  });
-});
-
 test('top level test', async (t) => {
-  await t.test('subtest 1', (t) => {
+  await t.test('subtest 1', (_t) => {
     assert.strictEqual(1, 1);
   });
 
-  await t.test('subtest 2', (t) => {
+  await t.test('subtest 2', (_t) => {
     assert.strictEqual(2, 2);
   });
 });
 
 // The skip option is used, but no message is provided.
-test('skip option', { skip: true }, (t) => {
+test('skip option', { skip: true }, (_t) => {
   // This code is never executed.
 });
 
 // The skip option is used, and a message is provided.
-test('skip option with message', { skip: 'this is skipped' }, (t) => {
+test('skip option with message', { skip: 'this is skipped' }, (_t) => {
   // This code is never executed.
 });
 
